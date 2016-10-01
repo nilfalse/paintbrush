97790b9f98c346f0e38f9ba453a871c25713a3be
Yaroslav Ilin <nilfalse@yandex.com>
2016-10-01 20:26:06 +0300

    2nd ver according to new requirements

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ May serve as example of using `canvas`.
 See https://nilfalse.com/PaintBrush/ for live demo.
 
 ## Original task
-Create software for drawing basic primitives: line and rectangle.
-Pen color and width must be configurable.
+Create software for drawing basic primitives: line, rectangle, circle and curve.
+Circle and rectangle can be filled by the color.
+Pen and fill colors, pen width must be configurable.
--- paintbrush.js
+++ paintbrush.js
@@ -1,17 +1,45 @@
 var paintbrush = (function(window, undefined) {
     // Примитивы
-    function Line(x1, y1, x2, y2) {
-        if (!(this instanceof Line)) {
-            return new Line(x1, y1, x2, y2);
-        }
+    function Curve(x1, y1) {
+        this.points = [];
+        this.x1 = x1;
+        this.y1 = y1;
+        this.canDraw = false;
+        this.color = null;
+        this.size = null;
+    }
+    Curve.hasSize = true;
+    Curve.prototype.capturePoint = function Curve_capturePoint(x, y) {
+        this.points.push({ x: x, y: y });
+        this.canDraw = true;
+    };
+    Curve.prototype.draw = function Curve_draw(ctx) {
+        ctx.lineWidth = this.size;
+        ctx.strokeStyle = this.color;
+        ctx.beginPath();
+        ctx.moveTo(this.x1, this.y1);
+        this.points.forEach(function(p) {
+            ctx.lineTo(p.x, p.y);
+        });
+        ctx.stroke();
+        ctx.closePath();
+    };
 
+    function Line(x1, y1, x2, y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
+        this.canDraw = false;
         this.color = null;
         this.size = null;
     }
+    Line.hasSize = true;
+    Line.prototype.capturePoint = function Line_capturePoint(x, y) {
+        this.x2 = x;
+        this.y2 = y;
+        this.canDraw = true;
+    };
     Line.prototype.draw = function Line_draw(ctx) {
         ctx.lineWidth = this.size;
         ctx.strokeStyle = this.color;
@@ -23,60 +51,129 @@ var paintbrush = (function(window, undefined) {
     };
 
     function Rect(x1, y1, x2, y2) {
-        if (!(this instanceof Rect)) {
-            return new Rect(x1, y1, x2, y2);
-        }
-
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
+        this.canDraw = false;
         this.color = null;
         this.size = null;
     }
+    Rect.hasSize = false;
+    Rect.prototype.capturePoint = function Rect_capturePoint(x, y) {
+        this.x2 = x;
+        this.y2 = y;
+        this.canDraw = true;
+    };
     Rect.prototype.draw = function Rect_draw(ctx) {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x1, this.y1, this.x2 - this.x1, this.y2 - this.y1);
     };
 
-    var serviceLocator = {
-        // Определяет примитив, который пользователь захотел нарисовать.
-        // В первом приближении алгоритм простой: с шифтом рисуем прямоугольники, иначе – линии.
-        getDrawableForEvent: function(e) {
-            return e.shiftKey ? Rect : Line;
-        }
+    function Circle(x1, y1, x2, y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.canDraw = false;
+        this.color = null;
+        this.size = null;
+    }
+    Circle.hasSize = false;
+    Circle.prototype.capturePoint = function Circle_capturePoint(x, y) {
+        this.x2 = x;
+        this.y2 = y;
+        this.canDraw = true;
+    };
+    Circle.prototype.drawEllipse = function Circle_drawEllipse(ctx, x, y, a, b) {
+        ctx.save();
+        ctx.beginPath();
+        ctx.translate(x, y);
+        ctx.scale(a / b, 1);
+        ctx.arc(0, 0, Math.abs(b), 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.restore();
+    };
+    Circle.prototype.draw = function Circle_draw(ctx) {
+        var x = (this.x1 + this.x2) / 2;
+        var y = (this.y1 + this.y2) / 2;
+        var a = (this.x2 - this.x1) / 2;
+        var b = (this.y2 - this.y1) / 2;
+        ctx.fillStyle = this.color;
+        this.drawEllipse(ctx, x, y, a, b);
+        ctx.fill();
     };
 
-    // Цвета в первой версии заданы внутри <select>, этот объект с ним работает.
-    var colorpalette = ({
-        // ссылка на <select>
-        _node: null,
-        // текущий выбранный цвет
+    // Конфигурация кисточки.
+    var brush = {
         currentColor: null,
+        currentSize: null,
+        selectedTool: null,
 
-        init: function colorpicker_init(node) {
+        toolbar: null,
+        tools: [],
+        selectTool: function tool_selectTool(tool) {
+            switch(tool) {
+            case 'curve': this.selectedTool = Curve; break;
+            case 'line': this.selectedTool = Line; break;
+            case 'rect': this.selectedTool = Rect; break;
+            case 'circle': this.selectedTool = Circle; break;
+            default: console.warn('unknown tool'); return;
+            }
+            this.toolbar.querySelector('#size-toolbar').style.visibility =
+                this.selectedTool.hasSize ? '' : 'hidden';
+        },
+        colors: [],
+        selectColor: function tool_selectColor(color) {
+            this.currentColor = color;
+            this.toolbar.querySelector('#current-color').style.backgroundColor = color;
+        },
+
+        init: function brush_init(toolbar) {
             var that = this;
-            this._node = node;
-            this.currentColor = node.value;
-            node.onchange = function colorpicker_handleonchange(e) {
-                that.currentColor = node.value;
+            this.toolbar = toolbar;
+
+            function unselectAll(items) {
+                items.forEach(function(item) { item.classList.remove('selected') });
+            }
+
+            this.tools = Array.from(toolbar.querySelectorAll('#toolbox li'));
+            this.tools.forEach(function(tool) {
+                var toolname = tool.dataset.tool;
+                tool.onclick = function(e) {
+                    unselectAll(that.tools);
+                    tool.classList.add('selected');
+                    that.selectTool(toolname, e);
+                };
+                if (!that.selectedTool) {
+                    tool.classList.add('selected');
+                    that.selectTool(toolname);
+                }
+            });
+
+            this.colors = Array.from(toolbar.querySelectorAll('#colorpalette li'));
+            this.colors.forEach(function(color) {
+                var colorname = color.className.replace('color__', '');
+                color.onclick = function(e) {
+                    unselectAll(that.colors);
+                    color.classList.add('selected');
+                    that.selectColor(colorname);
+                };
+                if (!that.currentColor) {
+                    color.classList.add('selected');
+                    that.selectColor(colorname);
+                }
+            });
+
+            var size = toolbar.querySelector('#size');
+            this.currentSize = size.valueAsNumber;
+            size.onchange = function (e) {
+                that.currentSize = size.valueAsNumber;
             };
+
             return this;
         }
-    }.init(document.getElementById('color')));
-
-    // Конфигурация кисточки. В первой версии только размер.
-    var brush = ({
-        size: null,
-        init: function brush_init(node) {
-            var that = this;
-            this.size = node.valueAsNumber;
-            node.onchange = function brush_handleonchange(e) {
-                that.size = node.valueAsNumber;
-            };
-            return this;
-        }
-    }.init(document.getElementById('size')));
+    };
 
     // Главный объект приложения (синглтон)
     var paintbrush = {
@@ -106,10 +203,9 @@ var paintbrush = (function(window, undefined) {
         },
 
         _onstart: function paintbrush__onstart(e) {
-            var Drawable = serviceLocator.getDrawableForEvent(e);
-            this.pending = new Drawable(e.offsetX, e.offsetY);
-            this.pending.color = colorpalette.currentColor;
-            this.pending.size = brush.size;
+            this.pending = new brush.selectedTool(e.offsetX, e.offsetY);
+            this.pending.color = brush.currentColor;
+            this.pending.size = brush.currentSize;
         },
         _onmove: function paintbrush__onmove(e) {
             if (!this.pending) {
@@ -118,8 +214,7 @@ var paintbrush = (function(window, undefined) {
 
             this.redraw();
             var drawable = this.pending;
-            drawable.x2 = e.offsetX;
-            drawable.y2 = e.offsetY;
+            drawable.capturePoint(e.offsetX, e.offsetY);
             drawable.draw(this.ctx);
         },
         _onstop: function paintbrush__onstop(e) {
@@ -129,13 +224,13 @@ var paintbrush = (function(window, undefined) {
             if (!drawable) {
                 return;
             }
-            if (!drawable.x2 || !drawable.y2) {
-                // простой клик не генерирует mousemove – рисовать нечего
+            if (!drawable.canDraw) {
                 return;
             }
             this.drawn.push(drawable);
         }
     };
 
+    brush.init(document.getElementById('toolbar'));
     return paintbrush.init(document.getElementById('canvas'));
 }(this));
